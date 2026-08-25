c8df5951f5e5c684781627fd6100efa24119d7a7
yellowflash8272 <lakshanravindu482@gmail.com>
2026-08-26 00:11:45 +0530

    Update slider_seiya.js

--- slider_seiya.js
+++ slider_seiya.js
@@ -1,4 +1,4 @@
-function initSaintSeiyaSlider() {
+document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelectorAll(".saint-seiya-slider").forEach(function(slider) {
 
@@ -9,31 +9,43 @@ function initSaintSeiyaSlider() {
 
         if (!slides.length) return;
 
-        slides.forEach(function(slide, i) {
-            slide.style.position = "absolute";
-            slide.style.top = "0";
-            slide.style.left = "0";
-            slide.style.width = "100%";
-            slide.style.display = i === 0 ? "block" : "none";
-        });
-
-        slider.style.position = "relative";
-
-        function changeSlide(n) {
+        function showSlide(newIndex) {
 
             slides[index].style.display = "none";
 
-            index += n;
-
-            if (index >= slides.length) {
-                index = 0;
-            }
-
-            if (index < 0) {
-                index = slides.length - 1;
-            }
+            index = newIndex;
 
             slides[index].style.display = "block";
+
+            updateHeight();
+        }
+
+        function changeSlide(n) {
+
+            let newIndex = index + n;
+
+            if (newIndex >= slides.length) {
+                newIndex = 0;
+            }
+
+            if (newIndex < 0) {
+                newIndex = slides.length - 1;
+            }
+
+            showSlide(newIndex);
+        }
+
+        function updateHeight() {
+
+            let currentSlide = slides[index];
+
+            if (currentSlide.complete && currentSlide.naturalHeight > 0) {
+                slider.style.height = currentSlide.offsetHeight + "px";
+            } else {
+                currentSlide.onload = function () {
+                    slider.style.height = currentSlide.offsetHeight + "px";
+                };
+            }
         }
 
         if (next) {
@@ -48,16 +60,18 @@ function initSaintSeiyaSlider() {
             };
         }
 
+        if (slides[0].complete) {
+            updateHeight();
+        } else {
+            slides[0].onload = updateHeight;
+        }
+
+        window.addEventListener("resize", updateHeight);
+
         setInterval(function () {
             changeSlide(1);
         }, 4000);
 
     });
 
-}
-
-if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", initSaintSeiyaSlider);
-} else {
-    initSaintSeiyaSlider();
-}
+});
