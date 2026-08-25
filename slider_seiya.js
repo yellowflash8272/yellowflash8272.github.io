aa70020c4599926c83f38989946b2d269555d5bd
yellowflash8272 <lakshanravindu482@gmail.com>
2026-08-26 00:20:43 +0530

    Update slider_seiya.js

--- slider_seiya.js
+++ slider_seiya.js
@@ -14,63 +14,38 @@
 
             if (!slides.length) return;
 
-            function updateHeight() {
-                let current = slides[index];
-
-                if (current) {
-                    slider.style.height = current.offsetHeight + "px";
-                }
-            }
-
-            function showSlide(newIndex) {
+            function changeSlide(direction) {
 
                 slides[index].style.display = "none";
 
-                index = newIndex;
+                index += direction;
+
+                if (index >= slides.length) {
+                    index = 0;
+                }
+
+                if (index < 0) {
+                    index = slides.length - 1;
+                }
 
                 slides[index].style.display = "block";
-
-                updateHeight();
-            }
-
-            function changeSlide(direction) {
-
-                let newIndex = index + direction;
-
-                if (newIndex >= slides.length) {
-                    newIndex = 0;
-                }
-
-                if (newIndex < 0) {
-                    newIndex = slides.length - 1;
-                }
-
-                showSlide(newIndex);
             }
 
             if (next) {
-                next.addEventListener("click", function () {
+                next.onclick = function () {
                     changeSlide(1);
-                });
+                };
             }
 
             if (prev) {
-                prev.addEventListener("click", function () {
+                prev.onclick = function () {
                     changeSlide(-1);
-                });
+                };
             }
-
-            slides[0].addEventListener("load", updateHeight);
-
-            if (slides[0].complete) {
-                updateHeight();
-            }
-
-            window.addEventListener("resize", updateHeight);
 
             setInterval(function () {
                 changeSlide(1);
-            }, 4000);
+            }, 6000);
 
         });
 
